9412cb0e0443e676ad0e6c5d82ff4ee5c146e490
William Lindner <wlindner522@gmail.com>
2014-10-12 23:19:59 -0700

    Changed branch to 'master' instead of 'gh-pages'.

--- Gruntfile.js
+++ Gruntfile.js
@@ -292,7 +292,7 @@ module.exports = function (grunt) {
       dist: {
         options: {
           remote: 'https://github.com/wlindner/wlindner.github.io.git',
-          branch: 'gh-pages',
+          branch: 'master',
           commit: true,
           push: true
         }
