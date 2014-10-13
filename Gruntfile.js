2e24d804dd3a71d04dc92502066d6860056c1656
William Lindner <wlindner522@gmail.com>
2014-10-12 22:53:03 -0700

    Change remote path.

--- Gruntfile.js
+++ Gruntfile.js
@@ -291,7 +291,7 @@ module.exports = function (grunt) {
     buildcontrol: {
       dist: {
         options: {
-          remote: 'git@github.com:wlindner/wlindner.github.io.git',
+          remote: '../',
           branch: 'gh-pages',
           commit: true,
           push: true
