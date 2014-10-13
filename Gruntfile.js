10c319e519b0bc220654a3816f28e80f460505c6
William Lindner <wlindner522@gmail.com>
2014-10-12 22:59:20 -0700

    Added remote repo URL.

--- Gruntfile.js
+++ Gruntfile.js
@@ -291,7 +291,7 @@ module.exports = function (grunt) {
     buildcontrol: {
       dist: {
         options: {
-          remote: '../',
+          remote: 'https://github.com/wlindner/wlindner.github.io.git',
           branch: 'gh-pages',
           commit: true,
           push: true
