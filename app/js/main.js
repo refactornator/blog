60fec10b5c5d737b432ea8b64990eaf6094757ea
William Lindner <wlindner522@gmail.com>
2014-10-19 19:47:58 -0700

    Don't use pjax for external links.

--- app/js/main.js
+++ app/js/main.js
@@ -22,43 +22,42 @@ document.onclick = function(e) {
     element = element.parentNode || null;
   }
 
-  if (element) {
+  if (element && element.host === document.location.host) {
     var container = 'site',
         containerEl = document.getElementById(container);
 
-      // Allow middle click (pages in new windows)
-      if (e.which > 1 || e.metaKey || e.ctrlKey) return;
+    // Allow middle click (pages in new windows)
+    if (e.which > 1 || e.metaKey || e.ctrlKey) return;
 
-      // Don't fire normal event
-      if(e.preventDefault){ e.preventDefault(); }else{ e.returnValue = false; }
+    // Don't fire normal event
+    if(e.preventDefault){ e.preventDefault(); }else{ e.returnValue = false; }
 
-      // Take no action if we are already on said page?
-      if (document.location.href === element.href) return false;
+    // Take no action if we are already on said page?
+    if (document.location.href === element.href) return false;
 
-      // handle the load.
-      Velocity(containerEl, {
-        opacity: [ 0, 1 ], translateY: 20, translateZ: 0
-      }, {
-        queue: false,
-        duration: 100,
-        complete: function() {
-          pjax.invoke({
-            url: element.href || '/',
-            container: containerEl,
-            parseLinksOnload: false,
-            complete: function(e) {
-              console.log('complete');
-              initMasonry();
+    // handle the load.
+    Velocity(containerEl, {
+      opacity: [ 0, 1 ], translateY: 20, translateZ: 0
+    }, {
+      queue: false,
+      duration: 100,
+      complete: function() {
+        pjax.invoke({
+          url: element.href || '/',
+          container: containerEl,
+          parseLinksOnload: false,
+          complete: function(e) {
+            initMasonry();
 
-              Velocity(containerEl, {
-                opacity: [ 1, 0 ], translateY: [ 0, 20 ], translateZ: 0
-              }, {
-                duration: 300
-              });
-            }
-          });
-        }
-      });
+            Velocity(containerEl, {
+              opacity: [ 1, 0 ], translateY: [ 0, 20 ], translateZ: 0
+            }, {
+              duration: 300
+            });
+          }
+        });
+      }
+    });
 
     return false; // prevent default action and stop event propagation
   }
